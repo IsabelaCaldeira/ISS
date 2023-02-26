2747c84a01e384302071aa6d337501e3c61b8383
Isabela <isabela.ribeiro@outlook.com>
2023-02-25 23:47:25 -0300

    displaing the astrounats names

--- main.py
+++ main.py
@@ -3,4 +3,18 @@ import turtle
 import urllib.request
 import time
 import webbrowser   
+import geocoder
 
+url = "http://api.open-notify.org/astro.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+file = open("iss.txt","w")
+file.write("Atualmente, possui" + str(result["number"]) + "astrounatas na estação espacial internacional: \n\n")
+
+people = result["people"]
+
+for p in people:
+    file.write(p['name'] + " - a bordo" + "\n")
+    
+ 
