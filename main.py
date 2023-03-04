41015ae233177580cf9c68182220c2554a0a4290
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-03-04 18:53:55 -0300

    displaying long and lat

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ url = "http://api.open-notify.org/astro.json"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
 
-#number of astronauts and their names
+#print astrounats
 file = open("iss.txt","w")
 file.write("Atualmente, possui" + str(result["number"]) + "astrounatas na estação espacial internacional: \n\n")
 
@@ -20,4 +20,9 @@ people = result["people"]
 for p in people:
     file.write(p['name'] + " - a bordo" + "\n")
     
- 
+# print long and lat
+g = geocoder.ip('me')
+file.write("\nA atual lang / long é: " + str(g.latlng))
+file.close()
+webbrowser.open("iss.txt")
+
