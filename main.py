49c5f19970da0baff4118dcd5b6ba06a29500641
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-03-04 19:02:31 -0300

    finishing the while

--- main.py
+++ main.py
@@ -39,3 +39,25 @@ iss.shape("iss.gif")
 iss.setheading(45)
 iss.penup()
 
+while True:
+    # load the current status of the ISS in real-time
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    # Extract the ISS location
+    location = result["iss_position"]
+    lat = location['latitude']
+    lon = location['longitude']
+
+    # Ouput lon and lat to the terminal
+    lat = float(lat)
+    lon = float(lon)
+    print("\nLatitude: " + str(lat))
+    print("\nLongitude: " + str(lon))
+
+    # Update the ISS location on the map
+    iss.goto(lon, lat)
+
+    # Refresh each 5 seconds
+    time.sleep(5)
